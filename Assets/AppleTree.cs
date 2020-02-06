c658726e7197739715b33e0dae7706474f4390e3
ccal9 <36898708+ccal9@users.noreply.github.com>
2020-02-05 20:55:41 -0500

    Basket prefab and tree script

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Set in Inspector")]
+
+    public GameObject applePrefab;
+
+    public float speed = 1f;
+
+    public float leftAndRightEdge = 10f;
+
+    public float chanceToChangeDirections = 0.1f;
+
+    public float secondsBetweenAppleDrops = 1f;
     void Start()
     {
         
