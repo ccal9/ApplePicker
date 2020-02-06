c84308838a0439d3e3c455a8cd2a0d8f3a10dac1
ccal9 <36898708+ccal9@users.noreply.github.com>
2020-02-05 23:06:31 -0500

    updated scripts
    game now knows of missed apples adn updates score

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -21,7 +21,15 @@ public class ApplePicker : MonoBehaviour
         }
 
     }
-
+    public void AppleDestroyed()
+    {
+        //	Destroy	all	of	the	falling	apples
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject tGO in tAppleArray)
+        {
+            Destroy(tGO);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public Text scoreGT;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGO.GetComponent<Text>();
+        scoreGT.text = "0";
     }
 
     // Update is called once per frame
@@ -20,5 +25,15 @@ public class Basket : MonoBehaviour
         pos.x = mousePos3D.x;
         this.transform.position = pos;
     }
-    
+    private void OnCollisionEnter(Collision coll)
+    {
+        GameObject collidedWith = coll.gameObject;
+        if(collidedWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+            int score = int.Parse(scoreGT.text);
+            score += 100;
+            scoreGT.text = score.ToString();
+        }
+    }
 }
