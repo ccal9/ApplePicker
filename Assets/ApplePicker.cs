9792f2e16c135237f76d87b187f5bca51a8aaa25
ccal9 <36898708+ccal9@users.noreply.github.com>
2020-02-05 23:25:51 -0500

    basket is destroyed upon miss

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -9,15 +10,18 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
     // Start is called before the first frame update
     void Start()
     {
+        basketList = new List<GameObject>();
         for (int i = 0; i < numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
 
     }
@@ -28,6 +32,14 @@ public class ApplePicker : MonoBehaviour
         foreach (GameObject tGO in tAppleArray)
         {
             Destroy(tGO);
+        }
+        int basketIndex = basketList.Count - 1;
+        GameObject tBasketGo = basketList[basketIndex];
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGo);
+        if(basketList.Count == 0)
+        {
+            SceneManager.LoadScene("_Scene_0");
         }
     }
     // Update is called once per frame
